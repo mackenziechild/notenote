b7f9359e6d9d50a4ce78efd958e143b301335a6b
Mackenzie Child <mackenzie.child@gmail.com>
2014-11-15 08:45:32 -0700

    Edit & Destroy Note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -26,9 +26,16 @@ class NotesController < ApplicationController
 	end
 
 	def update
+		if @note.update(note_params)
+			redirect_to @note
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
+		@note.destroy
+		redirect_to notes_path
 	end
 
 	private
