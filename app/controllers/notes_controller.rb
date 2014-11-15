346d67ba5c60058e8fac66c24da78ca70be610a9
Mackenzie Child <mackenzie.child@gmail.com>
2014-11-15 12:01:47 -0700

    Refactor notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,8 +3,7 @@ class NotesController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
-		@user = current_user
-		@notes = Note.where(user_id: @user)
+		@notes = Note.where(user_id: current_user)
 	end
 
 	def show
