fe89dbebdc9ae230a92965e0606ba84f4bae2aae
Mackenzie Child <mackenzie.child@gmail.com>
2014-11-15 08:12:23 -0700

    Generate Notes Controller

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,24 @@
+class NotesController < ApplicationController
+	def index
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def find_note
+	end
+
+	def note_params
+	end
+end
